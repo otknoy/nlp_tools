91786a08b2f096474e3241c9f8dea08f755c7219
otknoy <otknoy@gmail.com>
2015-10-11 20:29:24 +0900

    Fix argument

--- src/nlp/feature.py
+++ src/nlp/feature.py
@@ -5,12 +5,16 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import Normalizer
 
 def tf(doc):
+    docs = [' '.join(d) for d in docs]
+
     vectorizer = CountVectorizer(token_pattern=u'(?u)\\b\\w+\\b')
     features = vectorizer.fit_transform(doc)
     terms = vectorizer.get_feature_names()
     return features, terms
 
 def tfidf(docs):
+    docs = [' '.join(d) for d in docs]
+
     vectorizer = TfidfVectorizer(min_df=1, max_df=50, token_pattern=u'(?u)\\b\\w+\\b')
     features = vectorizer.fit_transform(docs)
     terms = vectorizer.get_feature_names()
@@ -32,7 +36,6 @@ if __name__ == '__main__':
             ['山下', 'さん', 'は', '山下', 'くん', 'と', '北海道', 'へ', '行く', 'た', '。'],
             ['山下', 'さん', 'は', '下山', 'くん', 'と', 'New York', 'へ', '行く', 'た', '。'],
             ['山上', 'さん', 'は', '山下', 'くん', 'と', '東京特許許可局', 'へ', '行く', 'た', '。'],]
-    docs = [' '.join(d) for d in docs]
 
     features, terms = tfidf(docs)
     print(terms)
